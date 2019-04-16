b951cfc81fb8d660f9d00caf7c3684d88bbd7354
hly <hly920928@gmail.com>
2019-04-16 22:49:39 +0800

    FrontEnd Tool Babel and Webpack I

--- webpack_test/src/entry_1.js
+++ webpack_test/src/entry_1.js
@@ -1,3 +1,6 @@
-require(".\\module_1.js");//even in same dir ,must have .\
+require("jquery");
+require(".\\module_1.js");
 require(".\\module_2.js");
+require(".\\pack_css.css");
 document.querySelector("#p1").innerHTML="p1_changed";
+$("#p4").text("using_jquery");
--- webpack_test/webpack.config.js
+++ webpack_test/webpack.config.js
@@ -1,10 +1,47 @@
 const path = require('path');
-
+const webpackBuildIn=require("webpack");
+const hwp=require("html-webpack-plugin");
+const cleaner=require("clean-webpack-plugin");
+const uglify=require("uglifyjs-webpack-plugin");
 module.exports = {
-  entry: './src/entry_1.js',
+  entry: {
+    entry_file:'./src/entry_1.js',
+   /*  m1:'./src/module_1.js',
+    m2:'./src/module_2.js' */
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundled.js'
+    filename: 'bundled.js',
+    publicPath:"./" //public file dir,relative to "path"
   },
+  module:{
+    rules:[
+      {
+        test:/\.css$/,
+        use:["style-loader","css-loader"]
+      },
+    {
+      test:/\.(jpg|jpeg|png|gif)$/,
+      use:{
+        loader:"file-loader"
+      }
+    },
+    {
+      test:require.resolve("jquery"),
+      use:{
+        loader:"expose-loader",
+        options:"$"
+      }
+    }
+    ]
+  },
+  plugins:[ 
+    new webpackBuildIn.BannerPlugin("hello from webpackBuildIn.BannerPlugin"),
+    new hwp({template:"./template_1.html"}),//relative to this file
+    new cleaner(),
+    new uglify()
+  ]
+     
+,
   mode:"development"
 };
